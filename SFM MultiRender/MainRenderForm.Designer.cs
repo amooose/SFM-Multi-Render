d94fdfd2c32157d8b365062e58b7adf6a2b91d5d
Dylan <35438152+amooose@users.noreply.github.com>
2025-05-14 18:15:47 -0400

    more dpi fixes

--- SFM MultiRender/MainRenderForm.Designer.cs
+++ SFM MultiRender/MainRenderForm.Designer.cs
@@ -426,8 +426,8 @@
             // 
             // SFM_MultiRender
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(672, 811);
             this.Controls.Add(this.creditLabel);
             this.Controls.Add(this.autoHideCheckbox);
